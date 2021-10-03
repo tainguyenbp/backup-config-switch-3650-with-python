139dd2f2a2468acd7e10bb3cc88d8e110e427e77
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-10-03 21:57:08 +0700

    Update backup-config-switch.py

--- backup-config-switch.py
+++ backup-config-switch.py
@@ -2,6 +2,8 @@
 import requests
 import os
 import sys
+import hvac
+import json
 from netmiko import ConnectHandler
 import time
 from datetime import datetime, timedelta
@@ -9,13 +11,36 @@ from netmiko.ssh_exception import NetMikoTimeoutException,NetMikoAuthenticationE
 from paramiko.ssh_exception import SSHException
 import subprocess
 
+# vault kv put secret/cisco_ios/host host='192.168.1.1'
+# vault kv put secret/cisco_ios/username username=tainguyenbp
+# vault kv put secret/cisco_ios/password password='tainn@passw0rd'
+# vault kv put secret/cisco_ios/port port=22
+# vault kv put secret/cisco_ios/secret secret=passw0rd
+
+client = hvac.Client(url=os.environ['VAULT_ADDR'], token=os.environ['VAULT_TOKEN'])
+
+
+read_response_host = client.secrets.kv.v2.read_secret_version(path='cisco_ios/host')
+read_response_username = client.secrets.kv.v2.read_secret_version(path='cisco_ios/username')
+read_response_password = client.secrets.kv.v2.read_secret_version(path='cisco_ios/password')
+read_response_port= client.secrets.kv.v2.read_secret_version(path='cisco_ios/port')
+read_response_secret= client.secrets.kv.v2.read_secret_version(path='cisco_ios/secret')
+
+host = read_response_host.get('data', '').get('data', '').get('host', '')
+username = read_response_username.get('data', '').get('data', '').get('username', '')
+password = read_response_password.get('data', '').get('data', '').get('password', '')
+port = read_response_port.get('data', '').get('data', '').get('port', '')
+secret = read_response_secret.get('data', '').get('data', '').get('secret', '')
+
+
+
 cisco_ios = {
     'device_type': 'cisco_ios',
-    'host':   '192.168.1.1',
-    'username': 'tainn',
-    'password': 'tainn@passw0rd',
-    'port' : 22,
-    'secret': 'tainn@passw0rd',
+    'host': host,
+    'username': username,
+    'password': password,
+    'port' : port,
+    'secret': secret,
 }  
 
 def _get_ymd_currently():
